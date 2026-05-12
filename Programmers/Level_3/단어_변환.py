12ed8086bc29d80b9f38c6d6e6f08e1b73a1ebd0
unknown <jaeung0129@gmail.com>
2026-05-12 11:36:18 +0900

    PGS [Level 3] 단어 변환

---
+++ Programmers/Level_3/단어_변환.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    
+    return check(begin, target, words)
+
+def check(begin, target, words):
+    queue = deque()
+    queue.append([begin, 0])
+    
+    while queue:
+        now, step = queue.popleft()
+        if now == target:
+            return step
+        
+        for word in words:
+            count = 0
+            for i in range(len(now)):
+                if now[i] != word[i]:
+                    count += 1
+            
+            if count == 1:
+                queue.append([word, step+1])
