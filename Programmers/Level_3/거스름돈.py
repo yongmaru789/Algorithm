b00833234d893e7f20e357eecd2434b19a5e3f0a
JAEUNGCHOI <jaeung0129@gmail.com>
2026-06-18 15:47:54 +0900

    PGS [Level 3] 거스름돈

---
+++ Programmers/Level_3/거스름돈.py
@@ -0,0 +1,10 @@
+def solution(n, money):
+    
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    
+    for m in money:
+        for price in range(m, n + 1):
+            dp[price] += dp[price - m]
+    
+    return dp[price] % 1000000007
